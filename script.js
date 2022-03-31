5b13d15c10c43e50a049afc7219176841a618ea4
niksa <niksaopfor@gmail.com>
2022-03-31 22:13:35 +0200

    Working on website

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 'use strict';
 
 // Bubbling event
-const navbar__list = document.querySelector('.navbar__list');
+const navbar__list = document.querySelectorAll('.navbar__list');
 const nav__bar = document.querySelector('.nav-bar');
 const list = document.querySelectorAll('li');
+const nav__link = document.querySelector('.nav__link');
 
-navbar__list.addEventListener('mouseover', function (event) {});
+nav__bar.addEventListener('mouseover', function (e) {
+  const mouseOvered = e.target;
+
+  document.querySelectorAll('.nav__link').forEach(li => {
+    li.style.opacity = 0.5;
+  });
+});
+
+// nav__bar.addEventListener('mouseout', function (e) {
+//   const mouseOvered = e.target;
+//   document.querySelectorAll('li').forEach(li => {
+//     mouseOvered.style.opacity = 1;
+//     li.style.opacity = 1;
+//   });
+// });
 
 // Sticky Navigation
